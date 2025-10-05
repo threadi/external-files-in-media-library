c1fe841190eff54e69ca7f4f5188970f695ad467
Thomas Zwirner <tzwirne@gmx.de>
2025-10-05 13:56:59 +0200

    add Imgur hint for localhost installations

--- app/Services/Services.php
+++ app/Services/Services.php
@@ -243,20 +243,20 @@ class Services {
 	 * @return array<array<string>>
 	 */
 	public function add_help( array $help_list ): array {
-		$content  = '<h1>' . __( 'Get files from external directory', 'external-files-in-media-library' ) . '</h1>';
-		$content .= '<p>' . __( 'The plugin allows you to integrate files from external directories into your media library.', 'external-files-in-media-library' ) . '</p>';
+		$content  = '<h1>' . __( 'Get files from external sources', 'external-files-in-media-library' ) . '</h1>';
+		$content .= '<p>' . __( 'The plugin allows you to integrate files from different external sources into your media library.', 'external-files-in-media-library' ) . '</p>';
 		$content .= '<h3>' . __( 'How to use', 'external-files-in-media-library' ) . '</h3>';
 		/* translators: %1$s will be replaced by a URL. */
-		$content .= '<ol><li>' . sprintf( __( 'Go to Media > <a href="%1$s">Import from directory</a>.', 'external-files-in-media-library' ), esc_url( Directory_Listing::get_instance()->get_view_directory_url( false ) ) ) . '</li>';
-		$content .= '<li>' . __( 'Choose the service you want to use.', 'external-files-in-media-library' ) . '</li>';
-		$content .= '<li>' . __( 'Enter your credentials to use the service for your external directory.', 'external-files-in-media-library' ) . '</li>';
-		$content .= '<li>' . __( 'Choose the files you want to import in your media library.', 'external-files-in-media-library' ) . '</li>';
+		$content .= '<ol><li>' . sprintf( __( 'Go to Media > <a href="%1$s">Add External Files</a>.', 'external-files-in-media-library' ), esc_url( Directory_Listing::get_instance()->get_view_directory_url( false ) ) ) . '</li>';
+		$content .= '<li>' . __( 'Choose the external source you want to use.', 'external-files-in-media-library' ) . '</li>';
+		$content .= '<li>' . __( 'Enter your credentials to use the service for your external directory, if requested.', 'external-files-in-media-library' ) . '</li>';
+		$content .= '<li>' . __( 'Choose the files you want to import into your media library.', 'external-files-in-media-library' ) . '</li>';
 		$content .= '</ol>';
 
 		// add help for the settings of this plugin.
 		$help_list[] = array(
 			'id'      => 'eml-directory',
-			'title'   => __( 'Using directories', 'external-files-in-media-library' ),
+			'title'   => __( 'Using external sources', 'external-files-in-media-library' ),
 			'content' => $content,
 		);
 
