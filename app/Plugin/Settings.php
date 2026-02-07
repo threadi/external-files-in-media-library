3b8b28ad0d491dc57078133b9f5c8254f3c26e9c
Thomas Zwirner <tzwirne@gmx.de>
2026-02-07 15:31:01 +0100

    move availability settings on own section

--- app/Plugin/Settings.php
+++ app/Plugin/Settings.php
@@ -418,12 +418,12 @@ class Settings {
 
 		// add setting.
 		$setting = $settings_obj->add_setting( 'eml_user_assign' );
-		$setting->set_section( $general_tab_main );
+		$setting->set_section( $advanced_tab_advanced );
 		$setting->set_type( 'integer' );
 		$setting->set_default( Users::get_instance()->get_first_administrator_user() );
 		$field = new Select();
 		$field->set_title( __( 'Assign new files to this user', 'external-files-in-media-library' ) );
-		$field->set_description( __( 'This is only a workaround if the actual user is not available (e.g., via WP CLI import). New files are normally assigned to the user who adds them.', 'external-files-in-media-library' ) );
+		$field->set_description( __( 'This is only a workaround if the actual user is not available (e.g., via WP CLI import or synchronisation). New files are normally assigned to the user who adds them.', 'external-files-in-media-library' ) );
 		$field->set_options( Roles::get_instance()->get_user_for_settings() );
 		$setting->set_field( $field );
 		$setting->set_help( '<p>' . $field->get_description() . '</p>' );
