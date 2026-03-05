2d0287ec0006ba980fbc759b5fa66e2f622d3b71
Thomas Zwirner <tzwirne@gmx.de>
2026-03-05 12:11:19 +0100

    switches DropBox to OAuth usage

--- admin/dropbox.js
+++ admin/dropbox.js
@@ -2,22 +2,28 @@
  * Save the access token for DropBox.
  */
 function efml_dropbox_connect() {
-  // get the access token from form.
-  let access_token = jQuery("#efml_dropbox_access_token").val();
+  // get the credentials from the form.
+  let api_key = jQuery("#efml_dropbox_api_key").val();
+  let api_secret = jQuery("#efml_dropbox_api_secret").val();
 
   // send request
   jQuery.ajax({
     url: efmlJsVarsDropBox.ajax_url,
     type: 'post',
     data: {
-      access_token: access_token,
-      action: 'efml_add_access_token',
-      nonce: efmlJsVarsDropBox.access_token_connect_nonce
+      api_key: api_key,
+      api_secret: api_secret,
+      action: 'efml_dropbox_setup_connection',
+      nonce: efmlJsVarsDropBox.dropbox_connect_nonce
     },
     error: function( jqXHR, textStatus, errorThrown ) {
       efml_ajax_error_dialog( errorThrown )
     },
     success: function( response ) {
+      if( response.data.url ) {
+        location.href = response.data.url;
+        return;
+      }
       efml_create_dialog( response.data );
     }
   });
