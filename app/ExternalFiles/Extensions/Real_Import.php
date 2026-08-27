e74140d8472b1923967cd475d9027619af8f11a3
Thomas Zwirner <tzwirne@gmx.de>
2026-08-27 20:28:57 +0200

    fixed the usage of "open new window" in attributes

--- app/ExternalFiles/Extensions/Real_Import.php
+++ app/ExternalFiles/Extensions/Real_Import.php
@@ -271,12 +271,12 @@ class Real_Import extends Extension_Base {
 				array(),
 				get_admin_url() . 'profile.php'
 			);
-			$text .= '<a href="' . esc_url( $url ) . '#efml-settings" target="_blank" title="' . esc_attr__( 'Go to user settings', 'external-files-in-media-library' ) . Helper::get_a11n_window_hint() . '"><span class="dashicons dashicons-admin-users"></span></a>';
+			$text .= '<a href="' . esc_url( $url ) . '#efml-settings" target="_blank" title="' . esc_attr__( 'Go to user settings', 'external-files-in-media-library' ) . '"><span class="dashicons dashicons-admin-users"></span></a>';
 		}
 
 		// add link to global settings.
 		if ( current_user_can( 'manage_options' ) ) {
-			$text .= '<a href="' . esc_url( \ExternalFilesInMediaLibrary\Plugin\Settings::get_instance()->get_url( 'eml_advanced' ) ) . '" target="_blank" title="' . esc_attr__( 'Go to plugin settings', 'external-files-in-media-library' ) . Helper::get_a11n_window_hint() . '"><span class="dashicons dashicons-admin-generic"></span></a>';
+			$text .= '<a href="' . esc_url( \ExternalFilesInMediaLibrary\Plugin\Settings::get_instance()->get_url( 'eml_advanced' ) ) . '" target="_blank" title="' . esc_attr__( 'Go to plugin settings', 'external-files-in-media-library' ) .'"><span class="dashicons dashicons-admin-generic"></span></a>';
 		}
 
 		// end the text.
